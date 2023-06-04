b9ed06b5322427163ca992b907b656aa68daf3a1
Virginia <viorbe20@gmail.com>
2023-06-04 21:55:06 +0200

    feat: carrusel funciona con embajadores. faltan los otros y el estilo

--- fab-idi/resources/views/panel-colaboradores.blade.php
+++ fab-idi/resources/views/panel-colaboradores.blade.php
@@ -133,6 +133,10 @@
             background: #1abc9c;
             border: 5px double;
         }
+
+        .item {
+            text-align: center;
+        }
     </style>
     <main id='main-panel-colaboradores'>
         @php
@@ -141,200 +145,62 @@
             $instituto = App\Models\User::where('id_colaborador', '4')->get();
         @endphp
 
-        <div id="myCarousel" class="carousel slide" data-ride="carousel">
+        <div id="carousel-embajadores" class="carousel slide" data-ride="carousel">
             <!-- Carousel indicators -->
             <ol class="carousel-indicators">
-                <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-                <li data-target="#myCarousel" data-slide-to="1"></li>
-                <li data-target="#myCarousel" data-slide-to="2"></li>
+                <li data-target="#carousel-embajadores" data-slide-to="0" class="active"></li>
+                <li data-target="#carousel-embajadores" data-slide-to="1"></li>
+                <li data-target="#carousel-embajadores" data-slide-to="2"></li>
             </ol>
             <!-- Wrapper for carousel items -->
             <div class="carousel-inner">
-                
+
                 <div class="carousel-item active">
                     <div class="row">
-                        <div class="col-sm-6">
-                            <div class="testimonial-wrapper">
-                                <div class="testimonial">Lorem ipsum dolor sit amet, consectetur adipiscing
-                                    elit. Nam eu sem tempor, varius quam at, luctus dui. Mauris magna metus,
-                                    dapibus nec turpis vel, semper malesuada ante, commodo iacul viverra.</div>
-                                <div class="media">
-                                    <img src="/examples/images/clients/1.jpg" class="mr-3" alt="">
-                                    <div class="media-body">
-                                        <div class="overview">
-                                            <div class="name"><b>Paula Wilson</b></div>
-                                            <div class="details">Media Analyst / SkyNet</div>
-                                            <div class="star-rating">
-                                                <ul class="list-inline">
-                                                    <li class="list-inline-item"><i class="fa fa-star"></i></li>
-                                                    <li class="list-inline-item"><i class="fa fa-star"></i></li>
-                                                    <li class="list-inline-item"><i class="fa fa-star"></i></li>
-                                                    <li class="list-inline-item"><i class="fa fa-star"></i></li>
-                                                    <li class="list-inline-item"><i class="fa fa-star-half-o"></i></li>
-                                                </ul>
-                                            </div>
-                                        </div>
+                        @foreach ($embajadores->take(2) as $embajador)
+                            <div class="col-sm-6">
+                                <div class="testimonial-wrapper">
+                                    <div class='testimonial-image'>
+                                        <img src="{{ $embajador->nombre }}" alt="Foto de perfil" class="profile-picture">
+                                    </div>
+                                    <div class='testimonial-name'>
+                                        <p>{{ $embajador->nombre }}</p>
+                                    </div>
+                                    <div class='testimonial-rrss'>
+                                        <p>{{ $embajador->twitter }}</p>
                                     </div>
                                 </div>
                             </div>
-                        </div>
-                        <div class="col-sm-6">
-                            <div class="testimonial-wrapper">
-                                <div class="testimonial">Vestibulum quis quam ut magna consequat faucibus.
-                                    Pellentesque eget mi suscipit tincidunt. Utmtc tempus dictum. Pellentesque
-                                    virra. Quis quam ut magna consequat faucibus, metus id mi gravida.</div>
-                                <div class="media">
-                                    <img src="/examples/images/clients/2.jpg" class="mr-3" alt="">
-                                    <div class="media-body">
-                                        <div class="overview">
-                                            <div class="name"><b>Antonio Moreno</b></div>
-                                            <div class="details">Web Developer / SoftBee</div>
-                                            <div class="star-rating">
-                                                <ul class="list-inline">
-                                                    <li class="list-inline-item"><i class="fa fa-star"></i></li>
-                                                    <li class="list-inline-item"><i class="fa fa-star"></i></li>
-                                                    <li class="list-inline-item"><i class="fa fa-star"></i></li>
-                                                    <li class="list-inline-item"><i class="fa fa-star"></i></li>
-                                                    <li class="list-inline-item"><i class="fa fa-star-o"></i>
-                                                    </li>
-                                                </ul>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
+                        @endforeach
                     </div>
                 </div>
 
-                <div class="carousel-item">
-                    <div class="row">
-                        <div class="col-sm-6">
-                            <div class="testimonial-wrapper">
-                                <div class="testimonial">Lorem ipsum dolor sit amet, consectetur adipiscing
-                                    elit. Nam eu sem tempor, varius quam at, luctus dui. Mauris magna metus,
-                                    dapibus nec turpis vel, semper malesuada ante, commodo iacul viverra.</div>
-                                <div class="media">
-                                    <img src="/examples/images/clients/3.jpg" class="mr-3" alt="">
-                                    <div class="media-body">
-                                        <div class="overview">
-                                            <div class="name"><b>Michael Holz</b></div>
-                                            <div class="details">Web Developer / DevCorp</div>
-                                            <div class="star-rating">
-                                                <ul class="list-inline">
-                                                    <li class="list-inline-item"><i class="fa fa-star"></i></li>
-                                                    <li class="list-inline-item"><i class="fa fa-star"></i></li>
-                                                    <li class="list-inline-item"><i class="fa fa-star"></i></li>
-                                                    <li class="list-inline-item"><i class="fa fa-star"></i></li>
-                                                    <li class="list-inline-item"><i class="fa fa-star-o"></i>
-                                                    </li>
-                                                </ul>
-                                            </div>
+                @foreach ($embajadores->chunk(2)->slice(1) as $chunk)
+                    <div class="carousel-item">
+                        <div class="row">
+                            @foreach ($chunk as $embajador)
+                                <div class="col-sm-6">
+                                    <div class="testimonial-wrapper">
+                                        <div class='testimonial-image'>
+                                            <img src="{{ $embajador->nombre }}" alt="Foto de perfil"
+                                                class="profile-picture">
+                                        </div>
+                                        <div class='testimonial-name'>
+                                            <p>{{ $embajador->nombre }}</p>
+                                        </div>
+                                        <div class='testimonial-rrss'>
+                                            <p>{{ $embajador->twitter }}</p>
                                         </div>
                                     </div>
                                 </div>
-                            </div>
-                        </div>
-                        <div class="col-sm-6">
-                            <div class="testimonial-wrapper">
-                                <div class="testimonial">Vestibulum quis quam ut magna consequat faucibus.
-                                    Pellentesque eget mi suscipit tincidunt. Utmtc tempus dictum. Pellentesque
-                                    virra. Quis quam ut magna consequat faucibus, metus id mi gravida.</div>
-                                <div class="media">
-                                    <img src="/examples/images/clients/4.jpg" class="mr-3" alt="">
-                                    <div class="media-body">
-                                        <div class="overview">
-                                            <div class="name"><b>Mary Saveley</b></div>
-                                            <div class="details">Graphic Designer / MarsMedia</div>
-                                            <div class="star-rating">
-                                                <ul class="list-inline">
-                                                    <li class="list-inline-item"><i class="fa fa-star"></i>
-                                                    </li>
-                                                    <li class="list-inline-item"><i class="fa fa-star"></i>
-                                                    </li>
-                                                    <li class="list-inline-item"><i class="fa fa-star"></i>
-                                                    </li>
-                                                    <li class="list-inline-item"><i class="fa fa-star"></i>
-                                                    </li>
-                                                    <li class="list-inline-item"><i class="fa fa-star-o"></i>
-                                                    </li>
-                                                </ul>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
+                            @endforeach
                         </div>
                     </div>
-                </div>
+                @endforeach
 
-                <div class="carousel-item">
-                    <div class="row">
-                        <div class="col-sm-6">
-                            <div class="testimonial-wrapper">
-                                <div class="testimonial">Lorem ipsum dolor sit amet, consectetur adipiscing
-                                    elit. Nam eu sem tempor, varius quam at, luctus dui. Mauris magna metus,
-                                    dapibus nec turpis vel, semper malesuada ante, commodo iacul viverra.</div>
-                                <div class="media">
-                                    <img src="/examples/images/clients/5.jpg" class="mr-3" alt="">
-                                    <div class="media-body">
-                                        <div class="overview">
-                                            <div class="name"><b>Martin Sommer</b></div>
-                                            <div class="details">SEO Analyst / RealSearch</div>
-                                            <div class="star-rating">
-                                                <ul class="list-inline">
-                                                    <li class="list-inline-item"><i class="fa fa-star"></i>
-                                                    </li>
-                                                    <li class="list-inline-item"><i class="fa fa-star"></i>
-                                                    </li>
-                                                    <li class="list-inline-item"><i class="fa fa-star"></i>
-                                                    </li>
-                                                    <li class="list-inline-item"><i class="fa fa-star"></i>
-                                                    </li>
-                                                    <li class="list-inline-item"><i class="fa fa-star-o"></i>
-                                                    </li>
-                                                </ul>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-sm-6">
-                            <div class="testimonial-wrapper">
-                                <div class="testimonial">Vestibulum quis quam ut magna consequat faucibus.
-                                    Pellentesque eget mi suscipit tincidunt. Utmtc tempus dictum. Pellentesque
-                                    virra. Quis quam ut magna consequat faucibus, metus id mi gravida.</div>
-                                <div class="media">
-                                    <img src="/examples/images/clients/6.jpg" class="mr-3" alt="">
-                                    <div class="media-body">
-                                        <div class="overview">
-                                            <div class="name"><b>John Williams</b></div>
-                                            <div class="details">Web Designer / UniqueDesign</div>
-                                            <div class="star-rating">
-                                                <ul class="list-inline">
-                                                    <li class="list-inline-item"><i class="fa fa-star"></i>
-                                                    </li>
-                                                    <li class="list-inline-item"><i class="fa fa-star"></i>
-                                                    </li>
-                                                    <li class="list-inline-item"><i class="fa fa-star"></i>
-                                                    </li>
-                                                    <li class="list-inline-item"><i class="fa fa-star"></i>
-                                                    </li>
-                                                    <li class="list-inline-item"><i class="fa fa-star-o"></i>
-                                                    </li>
-                                                </ul>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                
             </div>
         </div>
+
 
 
 
