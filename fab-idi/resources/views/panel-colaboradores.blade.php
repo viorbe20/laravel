9961a08f57347253ce7d5524e3fbf24fa5a8714c
Virginia <viorbe20@gmail.com>
2023-06-04 22:40:09 +0200

    feat: mete rrss en carrusel

--- fab-idi/resources/views/panel-colaboradores.blade.php
+++ fab-idi/resources/views/panel-colaboradores.blade.php
@@ -15,103 +15,9 @@
 
 @section('content')
     <style>
-        body {
-            font-family: "Open Sans", sans-serif;
-        }
-
-        h2 {
-            color: #000;
-            font-size: 26px;
-            font-weight: 300;
-            text-align: center;
-            position: relative;
-            margin: 30px 0 60px;
-        }
-
-        h2::after {
-            content: "";
-            width: 100px;
-            position: absolute;
-            margin: 0 auto;
-            height: 4px;
-            border-radius: 1px;
-            background: #1abc9c;
-            left: 0;
-            right: 0;
-            bottom: -20px;
-        }
-
-        .carousel .carousel-item {
-            color: #999;
-            overflow: hidden;
-            min-height: 120px;
-            font-size: 13px;
-        }
-
-        .carousel .media {
-            position: relative;
-            padding: 0 0 0 20px;
-        }
-
-        .carousel .media img {
-            width: 75px;
-            height: 75px;
-            display: block;
-            border-radius: 50%;
-        }
-
-        .carousel .testimonial-wrapper {
-            padding: 0 10px;
-        }
-
-        .carousel .testimonial {
-            color: #808080;
-            position: relative;
-            padding: 15px;
-            background: #f1f1f1;
-            border: 1px solid #efefef;
-            border-radius: 3px;
-            margin-bottom: 15px;
-        }
-
-        .carousel .testimonial::after {
-            content: "";
-            width: 15px;
-            height: 15px;
-            display: block;
-            background: #f1f1f1;
-            border: 1px solid #efefef;
-            border-width: 0 0 1px 1px;
-            position: absolute;
-            bottom: -8px;
-            left: 46px;
-            transform: rotateZ(-46deg);
-        }
-
-        .carousel .star-rating li {
-            padding: 0 2px;
-        }
-
-        .carousel .star-rating i {
-            font-size: 16px;
-            color: #ffdc12;
-        }
-
-        .carousel .overview {
-            padding: 3px 0 0 15px;
-        }
-
-        .carousel .overview .details {
-            padding: 5px 0 8px;
-        }
-
-        .carousel .overview b {
-            text-transform: uppercase;
-            color: #1abc9c;
-        }
-
         .carousel .carousel-indicators {
             bottom: -70px;
+            background: #1abc9c;
         }
 
         .carousel-indicators li,
@@ -133,10 +39,6 @@
             background: #1abc9c;
             border: 5px double;
         }
-
-        .item {
-            text-align: center;
-        }
     </style>
     <main id='main-panel-colaboradores'>
         @php
@@ -145,36 +47,48 @@
             $instituto = App\Models\User::where('id_colaborador', '4')->get();
         @endphp
 
-        <div id="carousel-embajadores" class="carousel slide" data-ride="carousel">
+        <div id="carousel-embajadores" class="carousel slide my-carousel" data-ride="carousel">
             <!-- Carousel indicators -->
             <ol class="carousel-indicators">
                 <li data-target="#carousel-embajadores" data-slide-to="0" class="active"></li>
                 <li data-target="#carousel-embajadores" data-slide-to="1"></li>
                 <li data-target="#carousel-embajadores" data-slide-to="2"></li>
             </ol>
-            <!-- Wrapper for carousel items -->
-            <div class="carousel-inner">
 
+            <div class="carousel-inner">
+                <h4>Embajadores</h4>
                 <div class="carousel-item active">
                     <div class="row">
                         @foreach ($embajadores->take(2) as $embajador)
                             <div class="col-sm-6">
                                 <div class="testimonial-wrapper">
                                     <div class='testimonial-image'>
-                                        <img src="{{ $embajador->nombre }}" alt="Foto de perfil" class="profile-picture">
+                                        <img src="{{ asset('images/usuarios/' . $embajador->imagen) }}" alt="Foto de perfil"
+                                            class="profile-picture">
                                     </div>
                                     <div class='testimonial-name'>
                                         <p>{{ $embajador->nombre }}</p>
                                     </div>
                                     <div class='testimonial-rrss'>
-                                        <p>{{ $embajador->twitter }}</p>
+                                        @if (!empty($embajador->twitter))
+                                            <a href="{{ $embajador->twitter }}"><i class="fa-brands fa-twitter"></i></a>
+                                        @endif
+                                        @if (!empty($embajador->instagram))
+                                            <a href="{{ $embajador->instagram }}"><i class="fa-brands fa-instagram"></i></a>
+                                        @endif
+                                        @if (!empty($embajador->linkedin))
+                                            <a href="{{ $embajador->linkedin }}"><i class="fa-brands fa-linkedin"></i></a>
+                                        @endif
                                     </div>
+
                                 </div>
                             </div>
                         @endforeach
                     </div>
                 </div>
 
+                {{--slice(1): Toma una porción de 2 (chunk(2)) después del primer grupo. 
+                En este caso, se omite el primer grupo de 2 elementos y se obtiene el resto de la colección.--}}
                 @foreach ($embajadores->chunk(2)->slice(1) as $chunk)
                     <div class="carousel-item">
                         <div class="row">
@@ -182,15 +96,24 @@
                                 <div class="col-sm-6">
                                     <div class="testimonial-wrapper">
                                         <div class='testimonial-image'>
-                                            <img src="{{ $embajador->nombre }}" alt="Foto de perfil"
+                                            <img src="{{ asset('images/usuarios/' . $embajador->imagen) }}" alt="Foto de perfil"
                                                 class="profile-picture">
                                         </div>
                                         <div class='testimonial-name'>
                                             <p>{{ $embajador->nombre }}</p>
                                         </div>
                                         <div class='testimonial-rrss'>
-                                            <p>{{ $embajador->twitter }}</p>
+                                            @if (!empty($embajador->twitter))
+                                                <a href="{{ $embajador->twitter }}"><i class="fa-brands fa-twitter"></i></a>
+                                            @endif
+                                            @if (!empty($embajador->instagram))
+                                                <a href="{{ $embajador->instagram }}"><i class="fa-brands fa-instagram"></i></a>
+                                            @endif
+                                            @if (!empty($embajador->linkedin))
+                                                <a href="{{ $embajador->linkedin }}"><i class="fa-brands fa-linkedin"></i></a>
+                                            @endif
                                         </div>
+    
                                     </div>
                                 </div>
                             @endforeach
@@ -200,10 +123,6 @@
 
             </div>
         </div>
-
-
-
-
 
 
     </main>
