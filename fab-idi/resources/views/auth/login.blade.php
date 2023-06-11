84bf1095047cb931de07e2f4e7e5e73b7409ac1c
Virginia <viorbe20@gmail.com>
2023-06-11 20:49:53 +0200

    añadido bot

--- fab-idi/resources/views/auth/login.blade.php
+++ fab-idi/resources/views/auth/login.blade.php
@@ -13,11 +13,19 @@
                 <h3 class="card-title">Login</h3>
             </div>
             <div class="card-body">
-                @if (Session::has('error'))
-                    <div class="alert alert-danger" role="alert">
-                        {{ Session::get('error') }}
-                    </div>
-                @endif
+                
+                {{-- Mensajes informativos de logueo --}}
+                @if (session('success'))
+                <div class="alert alert-success">
+                    {{ session('success') }}
+                </div>
+            @elseif (session('error'))
+                <div class="alert alert-danger">
+                    {{ session('error') }}
+                </div>
+            @endif
+
+
                 <form action="{{ route('login') }}" method="POST">
                     @csrf
                     <div class="mb-3">
@@ -30,13 +38,14 @@
                         @enderror
                     </div>
                     <div class="mb-3">
+                        
                         <label for="password" class="form-label">Password</label>
+                        <div style="display:flex">
                         <input type="password" name="password" id="password" class="form-control">
-                        @error('password')
-                            <div class="text-danger mt-1">
-                                {{ $message }}
-                            </div>
-                        @enderror
+                        <button type="button" id="togglePassword" class="btn btn-outline-success m-2" onclick="togglePasswordVisibility()">
+                            <i class="fas fa-eye"></i>
+                        </button>
+                        </div>
                     </div>´
                     <div class="mb-3">
                         <a href="{{ route('olvidar-contrasena') }}">He olvidado mi contraseña</a>
@@ -45,6 +54,7 @@
                         <button type="submit" class="btn btn-primary">Login</button>
                     </div>
                 </form>
+
             </div>
         </div>
     </div>
@@ -52,3 +62,18 @@
 
 </main>
 @endsection
+
+<script>
+    function togglePasswordVisibility() {
+        var passwordInput = document.getElementById("password");
+        var toggleButton = document.getElementById("togglePassword");
+        
+        if (passwordInput.type === "password") {
+            passwordInput.type = "text";
+            toggleButton.innerHTML = '<i class="fas fa-eye-slash"></i>';
+        } else {
+            passwordInput.type = "password";
+            toggleButton.innerHTML = '<i class="fas fa-eye"></i>';
+        }
+    }
+</script>
