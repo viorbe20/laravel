9e6065db0bd4d47e9e1c220bb6166ca44ab625f5
Virginia <viorbe20@gmail.com>
2023-06-12 22:24:06 +0200

    refact quienes somos con verión de maría

--- fab-idi/resources/views/quienes-somos.blade.php
+++ fab-idi/resources/views/quienes-somos.blade.php
@@ -1,9 +1,6 @@
 @extends('layouts.plantilla')
 
-@section('head')
-<link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css" />
-<script type="text/javascript" src="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
-@endsection
+@vite(['resources/js/altaUsuario.js'])
 
 @section('title', 'Quienes Somos')
 
@@ -12,7 +9,7 @@
 <main id='main-quienes-somos'>
     <section id="historia-red">
         <div id="historia">
-            <h2>Historia del FAB-IDI</h2>
+            <h2 class="titulo">HISTORIA DEL FAB-IDI</h2>
             <p>
                 Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores atque adipisci, molestias sequi
                 quaerat
@@ -33,71 +30,130 @@
                 culpa, libero id ducimus eligendi, consequuntur numquam rem nostrum facilis sit odit officia similique
                 soluta tenetur.
             </p>
-
 
         </div>
         <div id="img-red">
-            <img src="{{ asset('images/logo.png') }}" alt="logo-fab-idi">
+            <img src="{{ asset('img/colmena.png') }}" alt="colmena-fab-idi">
         </div>
     </section>
 
     <section class="formulario mt-5">
         <div class="form-header">
-            <h4>Inscripción a la red FAB-IDI</h4>
+            <h4>INSCRIPCIÓN A LA RED FAB-IDI</h4>
         </div>
 
-        <form class="form-body" action="mailto:maria14998@gmail.com" method="POST">
+        <form class="form-body" action="{{ route('quienes-somos') }}" method="POST" enctype="multipart/form-data">
             @csrf
             <div class="row">
                 <div class="mb-3 col">
-                    <label class="form-label" for="tipo">Tipo de usuario</label>
-                    <select class="form-select" name="tipo" id="tipo" required>
+                    <label class="form-label" for="tipoUsuario">TIPO DE USUARIO</label>
+                    <select class="form-select" name="tipoUsuario" id="form-select-tipo-usuario" required>
+                        <option value="" selected>Selecciona el tipo de usuario</option>
                         <option value="usuario">Usuario</option>
-                        <option value="entidad">Entidad (Instituto, Universidad, Empresa.)</option>
+                        <option value="entidad">Entidad (Instituto, Universidad, Empresa...)</option>
                     </select>
                 </div>
-                <div class="mb-3 col">
-                    <label class="form-label" for="nombre">Nombre</label>
-                    <input class="form-control" type="text" name="nombre" required>
+                <!-- USUARIO -->
+                <div id="usuario-campos" style="display: none;">
+                    <div class="row">
+                        <div class="mb-3 col">
+                            <label for="nombre">NOMBRE*</label>
+                            <input type="text" class="form-control required-usuario" name="nombre-usuario">
+                        </div>
+                        <div class="mb-3 col">
+                            <label for="apellidos">APELLIDOS*</label>
+                            <input type="text" class="form-control" name="apellidos-usuario">
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="mb-3 col">
+                            <label for="email">EMAIL*</label>
+                            <input type="email" class="form-control" name="email-usuario">
+                        </div>
+                        <div class="mb-3 col">
+                            <label for="telefono">TELÉFONO</label>
+                            <input type="phone" class="form-control" name="telefono-usuario">
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="mb-3 col">
+                            <label for="twitter">TWITTER</label>
+                            <input type="text" class="form-control" name="twitter-usuario">
+                        </div>
+                        <div class="mb-3 col">
+                            <label for="instagram">INSTAGRAM</label>
+                            <input type="text" class="form-control" name="instagram-usuario">
+                        </div>
+                        <div class="mb-3 col">
+                            <label for="linkedin">LINKEDIN</label>
+                            <input type="text" class="form-control" name="linkedin-usuario">
+                        </div>
+                    </div>
+                    <div class="mb-3 col">
+                        <label class="form-label" for="mensaje">MENSAJE</label>
+                        <textarea class="form-control" name="mensaje-usuario" id="mensaje-usuario" cols="20"
+                            rows="5"></textarea>
+                    </div>
                 </div>
-                <div class="mb-3 col">
-                    <label class="form-label" for="nombre">Apellidos</label>
-                    <input class="form-control" type="text" name="apellidos">
+                <!-- ENTIDAD -->
+                <div id="entidad-campos" style="display: none;">
+                    <div class="row">
+                        <div class="mb-3 col">
+                            <label for="nombre">NOMBRE*</label>
+                            <input type="text" class="form-control required-entidad" name="nombre-entidad">
+                        </div>
+                        <div class="mb-3 col">
+                            <label for="apellidos">REPRESENTANTE</label>
+                            <input type="text" class="form-control" name="representante-entidad">
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="mb-3 col">
+                            <label for="email">EMAIL*</label>
+                            <input type="email" class="form-control" name="email-entidad">
+                        </div>
+                        <div class="mb-3 col">
+                            <label for="telefono">TELÉFONO</label>
+                            <input type="phone" class="form-control" name="telefono-entidad">
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="mb-3 col">
+                            <label for="web">WEB</label>
+                            <input type="text" class="form-control" name="web-entidad">
+                        </div>
+                    </div>
+                    <div class="mb-3 col">
+                        <label class="form-label" for="mensaje">MENSAJE</label>
+                        <textarea class="form-control" name="mensaje-entidad" id="mensaje-entidad" cols="20"
+                            rows="5"></textarea>
+                    </div>
                 </div>
-            </div>
-            <div class="row">
-                <div class="mb-3 col">
-                    <label class="form-label" for="email">Email</label>
-                    <input class="form-control" type="email" name="email" required>
+                <div id="btn-crear-usuario" class="btn-submit mb-3">
+                    <input class="btn btn-principal" type="submit" value="Enviar">
                 </div>
-                <div class="mb-3 col">
-                    <label class="form-label" for="telefono">Teléfono</label>
-                    <input class="form-control" type="tel" name="telefono">
-                </div>
-            </div>
-            <div class="row">
-                <div class="mb-3 col">
-                    <label class="form-label" for="twitter">Twitter</label>
-                    <input class="form-control" type="text" name="twitter">
-                </div>
-                <div class="mb-3 col">
-                    <label class="form-label" for="instagram">Instagram</label>
-                    <input class="form-control" type="text" name="instagram">
-                </div>
-                <div class="mb-3 col">
-                    <label class="form-label" for="linkedin">Linkedin</label>
-                    <input class="form-control" type="text" name="linkedin">
-                </div>
-            </div>
-            <div class="mb-3 col">
-                <label class="form-label" for="mensaje">Mensaje</label>
-                <textarea class="form-control" name="mensaje" id="mensaje" cols="30" rows="10"></textarea>
-            </div>
-            <div class="mb-3">
-                <input class="btn btn-principal" type="submit" value="Enviar">
-            </div>
         </form>
     </section>
+
+    <div>
+    @if (isset($enviado))
+        @if ($enviado)
+            <div class="alert alert-success mt-5" role="alert">
+                <h4 class="alert-heading">¡Enhorabuena!</h4>
+                <p>El formulario se ha enviado correctamente.</p>
+                <hr>
+                <p class="mb-0">Nos pondremos en contacto con usted cuando su solicitud sea aceptada. Gracias.</p>
+            </div>
+        @else
+            <div class="alert alert-danger mt-5" role="alert">
+                <h4 class="alert-heading">¡Error!</h4>
+                <p>El formulario no se ha enviado correctamente.</p>
+                <hr>
+                <p class="mb-0">Por favor, inténtelo de nuevo más tarde.</p>
+            </div>
+        @endif
+    @endif
+    </div>
 
 
 </main>
