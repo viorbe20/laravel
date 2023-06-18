6312b57060bd9eff6ae5ab39351872fa54224b79
Virginia <viorbe20@gmail.com>
2023-06-18 17:57:04 +0200

    estilo página premios y responsive

--- fab-idi/resources/views/mostrar-premios.blade.php
+++ fab-idi/resources/views/mostrar-premios.blade.php
@@ -7,27 +7,18 @@
 
     <main id='main-premios'>
         <div class="gallery">
-            @php
-                $columnCount = 4; // Número de columnas deseadas
-                $rowSize = ceil(count($premios) / $columnCount); // Tamaño de cada fila
-            @endphp
 
-            @foreach ($premios->chunk($rowSize) as $row)
-                <div class="gallery__row">
-                    @foreach ($row as $premio)
-                        <div class="card">
-                            <a href="{{ $premio->url }}" target="_blank" class="gallery__link">
-                                <figure class="gallery__thumb">
-                                    <img src="{{ asset('img/premios/' . $premio->imagen) }}" alt=""
-                                        class="gallery__image">
-                                    <figcaption class="gallery__caption">{{ $premio->titulo }}</figcaption>
-                                </figure>
-                            </a>
-							<h4 class="">{{ $premio->titulo }}</h4>
-                        </div>
-                    @endforeach
+            @foreach ($premios as $premio)
+                <div class="card">
+                    <a href="{{ $premio->url }}" target="">
+                        <figure class="gallery__thumb">
+                            <img src="{{ asset('img/premios/' . $premio->imagen) }}" alt="" class="gallery__image">
+                        </figure>
+                        <h4 class="">{{ $premio->titulo }}</h4>
+                    </a>
                 </div>
             @endforeach
+
         </div>
 
     </main>
